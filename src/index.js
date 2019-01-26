fab80bdd3ec361a1c5e977768eea1d70ebe1654b
Óscar Cortés Cordero <oscarcortesc12@gmail.com>
2019-01-25 23:41:42 -0600

    Initial files to test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,41 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { combineReducers, createStore } from 'redux';
+
+import { Provider } from 'react-redux';
+
+function productsReducer(state = [], action){
+  return state;
+}
+
+function userReducer(state = "", {type, payload}){
+  switch(type){
+    case 'updateUser':
+      return payload;
+  }
+  return state;
+}
+
+const allReducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+} )
+
+const store = createStore(
+  allReducers,
+  {
+  products: [ {name: "iPhone"} ],
+  user: "Usuario Anonimo"
+  },
+
+  window.devToolsExtension && window.devToolsExtension()
+);
+console.log(store.getState());
+
+
+
+ReactDOM.render(<Provider store = {store}>  <App /> </Provider> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
